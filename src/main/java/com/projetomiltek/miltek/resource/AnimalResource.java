96fe08a2c089dced36eaabafc3ffb9357c9e3fa1
Brenno <ubrennz@gmail.comm>
2026-06-30 16:44:31 -0300

    feat: Insert e findAll

--- src/main/java/com/projetomiltek/miltek/resource/AnimalResource.java
+++ src/main/java/com/projetomiltek/miltek/resource/AnimalResource.java
@@ -1,26 +1,28 @@
 package com.projetomiltek.miltek.resource;
 
 import com.projetomiltek.miltek.model.Animal;
+import com.projetomiltek.miltek.service.AnimalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/animais")
 public class AnimalResource {
-//    @GetMapping
-//    public ResponseEntity<List<Animal>> findAll() {
-//        List<Animal> animais = new ArrayList<>();
-//
-//        animais.add(new Animal(1, 1, "mimosa", "vaca", "M", LocalDate.parse("2024-06-14"), "a", null, false, LocalDateTime.now()));
-//        animais.add(new Animal(2, 1, "kaka", "vaca", "F", LocalDate.parse("2024-04-20"), "b", null, true, LocalDateTime.now()));
-//
-//        return ResponseEntity.ok().body(animais);
-//    }
+    @Autowired
+    private AnimalService animalService;
+
+    @PostMapping
+    public ResponseEntity<Animal> insert(@RequestBody Animal animal) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(animalService.insert(animal));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Animal>> findAll() {
+        List<Animal> animais = animalService.findAll();
+        return ResponseEntity.ok().body(animais);
+    }
 }
